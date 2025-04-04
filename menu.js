fbabfd9efca5a3f9870d245fcbadc0fc1688e252
italocampo <italosouzacampos99@gmail.com>
2025-04-04 00:41:03 -0300

    inicio-backend

--- menu.js
+++ menu.js
@@ -1,3 +1,23 @@
+document.getElementById('formulario').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const dados = Object.fromEntries(formData.entries());
+    try{
+        const resposta = await fetch('http://localhost:3000/cadastro', {
+            method: 'POST',
+            headers: {'Content-type': 'application/json' },
+            body: JSON.stringify(dados)
+        });
+        const resultado = await resposta.json();
+        alert(resultado.message);
+    } catch (erro) {
+        console.error('Erro ao enviar o cadastro: ', erro);
+        alert('Erro ao enviar cadastro. ');
+    }
+});
+
+
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay')
